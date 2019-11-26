d3ebb4429527949ec42955e05c1dca4132c4c4fb
Lee Brenner <lbrenner@alegion.com>
2019-11-26 16:09:07 -0600

    adding support for shape and spacing theme objects

--- style-dictionary/build.js
+++ style-dictionary/build.js
@@ -1,13 +1,46 @@
-const StyleDictionary = require('style-dictionary');
+const StyleDictionary = require('style-dictionary').extend(__dirname + '/config.json');
+const fs = require('fs');
+const _ = require('lodash');
 
 console.log('Design Tokenizer build started...');
 console.log('\n==============================================');
 
+// FORMATTER
 
-// REGISTER THE CUSTOM TRANFORMS
+const spacingTemplate = _.template( fs.readFileSync('templates/spacing-objects.template') );
+const shapeTemplate = _.template( fs.readFileSync('templates/shape-objects.template') );
+
+StyleDictionary.registerFormat({
+  name: 'custom/spacing-objects',
+  formatter: spacingTemplate
+});
+
+StyleDictionary.registerFormat({
+  name: 'custom/shape-objects',
+  formatter: shapeTemplate
+});
+
+
+// FILTERS
+
+StyleDictionary.registerFilter({
+  name: 'isSpacer',
+  matcher: function(prop) {
+    return prop.path.includes("spacer");
+  }
+})
+
+StyleDictionary.registerFilter({
+  name: 'isRadius',
+  matcher: function(prop) {
+    return prop.path.includes("corner-radius");
+  }
+})
+
+// TRANFORMS
 
 StyleDictionary.registerTransform({
-  name: 'custom-size/px', 
+  name: 'custom/size-px', 
   type: 'value',
   matcher: function(prop) {
       return prop.path.includes("size") || 
@@ -22,10 +55,34 @@ StyleDictionary.registerTransform({
   }
 });
 
+StyleDictionary.registerTransform({
+  name: 'custom/name-nodash',
+  type: 'name',
+  transformer: function(prop) {
+    let res = prop.name.split("-");
+    if (res.length > 1) {
+      var str = res[0];
+      for (i = 1; i < res.length; i++) {
+        str = str + res[i].charAt(0).toUpperCase() + res[i].substring(1);
+      }
+      return str;
+    }
+    else {
+      return prop.name;
+    }
+  }
+});
+
+// TRANFORM GROUPS
 
 StyleDictionary.registerTransformGroup({
   name: 'custom/css',
-  transforms: ["attribute/cti", "name/cti/kebab", "custom-size/px", "color/css"]
+  transforms: ["attribute/cti", "name/cti/kebab", "custom/size-px", "color/css"]
+});
+
+StyleDictionary.registerTransformGroup({
+  name: 'custom/javascript',
+  transforms: ["custom/name-nodash"]
 });
 
 // APPLY THE CONFIGURATION
